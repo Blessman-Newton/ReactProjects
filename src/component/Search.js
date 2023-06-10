ef5ab5132d1a165df7eecce0182c6b466bdf3e35
Blessman-Newton <blessmannewton0@gmail.com>
2023-06-10 19:16:02 +0100

    Add the search form

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,9 +1,12 @@
 import './Search.css'
 function Search() {
     return (
-        <>
-        
-        </>
+       <form action="">
+            <input type="text" placeholder="City/Street" />
+            <input type="text" placeholder="Property Type"/>
+            <input type="text" placeholder="Price Range"/>
+            <button>Search</button>
+       </form>
     );
 };
 
