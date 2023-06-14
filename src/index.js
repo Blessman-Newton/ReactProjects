2894c780c928900139d31f20f1b337b46008378b
Blessman-Newton <blessmannewton0@gmail.com>
2023-06-14 20:41:50 +0100

    Add react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
 import './main.css';
 import App from './App';
+import About from './Pages/About';
+
+const router = createBrowserRouter ([
+  {
+  path: '/',
+  element: <App />,
+  },
+  {
+  path: '/about',
+  element: <About />,
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
