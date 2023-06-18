b9458f75de84f72a5d251a177d125dccf85f8ae3
Blessman-Newton <blessmannewton0@gmail.com>
2023-06-18 03:33:06 +0100

    change the btn color

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,11 +1,13 @@
 import './Search.css';
 
 function Search() {
+    const inputType = "text"
     return (
        <form action="">
-            <input type="text" placeholder="City/Street" />
-            <input type="text" placeholder="Property Type"/>
-            <input type="text" placeholder="Price Range"/>
+            {/* <h4>Premium Properties At Your Hands</h4> */}
+            <input type={inputType} placeholder="City/Street" />
+            <input type={inputType} placeholder="Property Type" />
+            <input type={inputType} placeholder="Price Range" />
             <button>Search</button>
        </form>
     );
